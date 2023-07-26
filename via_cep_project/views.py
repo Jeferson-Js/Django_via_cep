bd397bab151870ede1cc4b5120a6e17c235fdf6d
Jeferson-Js <oliveira.jeferson.jso@gmail.com>
2023-07-26 18:07:47 -0300

    Atualização

--- via_cep_project/views.py
+++ via_cep_project/views.py
@@ -1,15 +1,13 @@
+# cep_api/views.py
+import requests
 from django.shortcuts import render
-import requests 
 from django.http import JsonResponse
 
-
-# Create your views here.
 def get_cep(request):
-    if request.method =='GET' and 'cep' in request.GET:
+    if request.method == 'GET' and 'cep' in request.GET:
         cep = request.GET['cep']
         url = f'https://viacep.com.br/ws/{cep}/json/'
         response = requests.get(url)
         data = response.json()
-        show_result = not data.get('erro')
         return JsonResponse(data)
     return render(request, 'home/index.html')
