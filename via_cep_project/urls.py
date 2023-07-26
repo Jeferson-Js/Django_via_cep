02e362dea2816281d714150ed2dc93851ce12eab
Jeferson-Js <oliveira.jeferson.jso@gmail.com>
2023-07-26 16:49:41 -0300

    The aplication consult of the cep in this project I use django

---
+++ via_cep_project/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path 
+from . import views 
+
+urlpatterns = [
+    path('get_cep/', views.get_cep, name='get_cep')
+]
---
+++ via_cep_project/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+import requests 
+from django.http import JsonResponse
+
+
+# Create your views here.
+def get_cep(request):
+    if request.method =='GET' and 'cep' in request.GET:
+        cep = request.GET['cep']
+        url = f'https://viacep.com.br/ws/{cep}/json/'
+        response = requests.get(url)
+        data = response.json()
+        return JsonResponse(data)
+    return render(request, 'home/index.html')
